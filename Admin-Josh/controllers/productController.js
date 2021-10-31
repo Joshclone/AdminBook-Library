330cae1c91efdb2a5fd4e038a820bc59eee0861f
Nwaeze Joshua <nwaezejoshua@gmail.com>
2021-10-31 21:00:13 +0100

    Add files via upload

---
+++ Admin-Josh/controllers/productController.js
@@ -0,0 +1,116 @@
+const Product = require("../models/productModel");
+
+const { getPostData } = require("../utils");
+
+///    Gets All book in the library database
+// via  GET /api/library
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//These function    Gets Single book from the library database
+//    GET /api/library/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// @desc    Create a Product
+// @route   POST /api/products
+async function createProduct(req, res) {
+  try {
+    const body = await getPostData(req);
+
+    const { name, description, price } = JSON.parse(body);
+
+    const product = {
+      name,
+      description,
+      price,
+    };
+
+    const newProduct = await Product.create(product);
+
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(newProduct));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//     Update a Product
+//    PUT /api/products/:id
+//Here we  Update a Book
+//    PUT /api/library/:id
+async function updateProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      const body = await getPostData(req);
+
+      const { name, description, price } = JSON.parse(body);
+
+      const productData = {
+        name: name || product.name,
+        description: description || product.description,
+        price: price || product.price,
+      };
+
+      const updProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(updProduct));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+///    Delete Book
+//    DELETE /api/library/:id
+async function deleteProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      await Product.remove(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: `Product ${id} removed` }));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
